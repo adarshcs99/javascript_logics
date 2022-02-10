1722437faab5de997f5b9881c2f23359e3406ab3
adarshcs99 <adarshsinghchauhan@cedcoss.com>
2022-02-10 09:19:15 +0530

    json object added

--- src/task2_js.js
+++ src/task2_js.js
@@ -1,9 +1,8 @@
 var mall = {
-    Price_id:[],
-    Price_name:[],
-    Price:[]
+    "Price_id":[],
+    "Price_name":[],
+    "Price":[]
 }; 
-
 var table_add = "";
 var index_increase = 0;
 function dis()
@@ -21,9 +20,9 @@ function dis()
    if(index_increase!=0)
    {
        let flag=0;
-       for(var i=0;i<mall.Price_id.length;i++)
+       for(var i=0;i<mall["Price_id"].length;i++)
        {
-           if(mall.Price_id[i] == id)
+           if(mall["Price_id"][i] == id)
            {
               alert("id already exsist");
               flag=1;
@@ -32,27 +31,28 @@ function dis()
        }
        if(!flag)
        {
-            mall.Price_id[index_increase] = id;
-            mall.Price_name[index_increase] = name;
-            mall.Price[index_increase] = pri;
+            mall["Price_id"][index_increase] = id;
+            mall["Price_name"][index_increase] = name;
+            mall["Price"][index_increase] = pri;
 
             table_add+=`<tr id=${index_increase}>`;
             
-            document.getElementById("table_show").innerHTML = table_add+=`<td>${mall.Price_id[index_increase]}</td><td>${mall.Price_name[index_increase]}</td><td>${mall.Price[index_increase]}</td></tr>`;
+            document.getElementById("table_show").innerHTML = table_add+=`<td>${mall["Price_id"][index_increase]}</td><td>${mall["Price_name"][index_increase]}</td><td>${mall["Price"][index_increase]}</td></tr>`;
             index_increase+=1;
        }
        
    }
    else{
     
-        mall.Price_id[index_increase] = id;
-        mall.Price_name[index_increase] = name;
-        mall.Price[index_increase] = pri;
+         mall["Price_id"][index_increase] = id;
+            mall["Price_name"][index_increase] = name;
+            mall["Price"][index_increase] = pri;
 
-        table_add+=`<tr id=${index_increase}>`;
-        
-        document.getElementById("table_show").innerHTML = table_add+=`<td>${mall.Price_id[index_increase]}</td><td>${mall.Price_name[index_increase]}</td><td>${mall.Price[index_increase]}</td></tr>`;
-        index_increase+=1;
+            table_add+=`<tr id=${index_increase}>`;
+            
+            document.getElementById("table_show").innerHTML = table_add+=`<td>${mall["Price_id"][index_increase]}</td><td>${mall["Price_name"][index_increase]}</td><td>${mall["Price"][index_increase]}</td></tr>`;
+            index_increase+=1;
+       
            
    }
     
